100b6cb168a0bb3805d39621c9c8bc31720e1531
mini <mini@blocks.imagemaker.fi>
2018-09-05 22:39:21 +0300

    Regenerated JS files

--- driver/BarcoPW392.js
+++ driver/BarcoPW392.js
@@ -1,7 +1,10 @@
 var __extends = (this && this.__extends) || (function () {
-    var extendStatics = Object.setPrototypeOf ||
-        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    }
     return function (d, b) {
         extendStatics(d, b);
         function __() { this.constructor = d; }
@@ -92,6 +95,7 @@ define(["require", "exports", "driver/NetworkProjector", "system_lib/Metadata"],
                 this.requestFinished();
             }
         };
+        var BarcoPW392_1;
         BarcoPW392.replyParser = /\%\d* (\S*) (\!?)(\d*)/;
         BarcoPW392.kMaxInput = 25;
         __decorate([
@@ -105,7 +109,6 @@ define(["require", "exports", "driver/NetworkProjector", "system_lib/Metadata"],
             __metadata("design:paramtypes", [Object])
         ], BarcoPW392);
         return BarcoPW392;
-        var BarcoPW392_1;
     }(NetworkProjector_1.NetworkProjector));
     exports.BarcoPW392 = BarcoPW392;
 });
--- driver/ChristiePerformance.js
+++ driver/ChristiePerformance.js
@@ -1,7 +1,10 @@
 var __extends = (this && this.__extends) || (function () {
-    var extendStatics = Object.setPrototypeOf ||
-        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    }
     return function (d, b) {
         extendStatics(d, b);
         function __() { this.constructor = d; }
@@ -150,6 +153,7 @@ define(["require", "exports", "system_lib/Driver", "system_lib/Metadata"], funct
             }
             cmdBuf.push(checksum);
         };
+        var ChristiePerformance_1;
         ChristiePerformance.kMinInput = 1;
         ChristiePerformance.kMaxInput = 16;
         ChristiePerformance.kLengthIx = 5;
@@ -175,7 +179,6 @@ define(["require", "exports", "system_lib/Driver", "system_lib/Metadata"], funct
             __metadata("design:paramtypes", [Object])
         ], ChristiePerformance);
         return ChristiePerformance;
-        var ChristiePerformance_1;
     }(Driver_1.Driver));
     exports.ChristiePerformance = ChristiePerformance;
 });
