5957072f387be64008223be5c09d4fb8670b70e5
Mika Raunio <mika@diago.global>
2025-01-28 07:13:25 +0200

    Add Tripla URL and zones in anticipation of install

--- user/IiwariWSClient.ts
+++ user/IiwariWSClient.ts
@@ -2,11 +2,53 @@ import {Script, ScriptEnv} from "system_lib/Script";
 import {property, resource} from "system_lib/Metadata";
 import { SimpleWebsocket, WebsocketConnection, TextMessage } from "system/SimpleWebsocket";
 
+/*
+
+{
+  "zones": [
+    {
+      "id": "01937754-1f08-88b2-9f9e-0d2772e16582",
+      "name": "Paikannusalue"
+    },
+    {
+      "id": "01937757-eb43-c8c8-8812-b8aa0ba9768c",
+      "name": "Trigger1"
+    },
+    {
+      "id": "01937758-b940-9b3f-8122-058e92efef0b",
+      "name": "Trigger2"
+    },
+    {
+      "id": "01937758-e2e7-294f-5cd0-36168e5729a2",
+      "name": "Trigger3"
+    },
+    {
+      "id": "01937759-48bf-4012-2225-49b653b93247",
+      "name": "Messut"
+    },
+    {
+      "id": "0193775a-042a-2efa-e5e8-f5dea9b7f330",
+      "name": "Materials"
+    },
+    {
+      "id": "0193775a-5003-ce40-18e2-b32b8a15c5ea",
+      "name": "Moving screen"
+    },
+    {
+      "id": "0193775a-a067-b818-9d85-b7216020c229",
+      "name": "Screens"
+    }
+  ]
+}
+
+*/
+
 const RECONN_DELAY_MS = 2.5 * 1000;
 const HEARTBEAT_INTERVAL_MS = 0;
 const RECEIVE_TIMEOUT_MS = 30 * 1000;
-const URL = 'wss://dash.iiwari.cloud/api/v1/sites/016fd235-8e10-1486-23f6-5e6cc6f4827b/stream?events=20,21'
-// const URL = 'ws://192.168.2.245:8123/'
+const URL = 'wss://dash.iiwari.cloud/api/v1/sites/016fd235-8e10-1486-23f6-5e6cc6f4827b/stream?events=20,21'  // Iiwari Snowpolis Test
+// const URL = 'wss://dash.iiwari.cloud/api/v1/sites/01937750-6649-c7b2-cb9c-81305a6e45c3/stream?events=20,21'  // Tripla
+// const URL = 'ws://192.168.2.245:8123/'  // Local wscat test
 const HEADERS = {
 	'Authorization': 'Bearer c7IIiWxOXC6jWwSPDvSWDKf5lfEUcsR79djeK5T3ScRKOMWFy4hVhU5N3l5PaOsi7VsUeXF3i7o8yfcTaB',
 }
